1f1f2f80fb16c1ad2cef14d1ac7ff732323b894e
Angel <angelbt91@gmail.com>
2020-01-11 18:11:23 +0100

    Scrapping con puppeeteer y varios scrolls

--- pl-scrapper-2.js
+++ pl-scrapper-2.js
@@ -1,10 +1,7 @@
-const axios = require('axios');
 const cheerio = require('cheerio');
-
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
-
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -21,9 +18,8 @@ db.once('open', function () {
     // paso 2: crear el modelo de un tweet
     var Tweet = mongoose.model('Tweet', tweetSchema);
 
-    // paso 3: crear los objetos a guardar en la bdd
+    // paso 3: cargar el chrome headless y hacer scroll y scroll
     const url = 'https://twitter.com/realdonaldtrump?lang=en';
-
 
     const puppeteer = require('puppeteer');
 
@@ -33,20 +29,31 @@ db.once('open', function () {
         await page.setViewport({width: 1366, height: 768});
         await page.goto('https://twitter.com/realdonaldtrump?lang=en');
 
-        // TODO: HACER SCROLL ANTES DE GUARDAR EL HTML DE LA PAGINA
+        await page.evaluate( () => {
+            window.scrollBy(0, 50000);
+        });
+        await page.waitFor(4000);
 
-        // !!!!!!!!!!!
+        await page.evaluate( () => {
+            window.scrollBy(0, 50000);
+        });
+        await page.waitFor(4000);
+
+        await page.evaluate( () => {
+            window.scrollBy(0, 50000);
+        });
+        await page.waitFor(4000);
 
         let html = await page.$eval('.ProfilePage', el => el.innerHTML);
-        const $ = cheerio.load(html);
-        const tweets = $('.tweet');
 
         await browser.close();
 
+        // paso 4: para cada tweet, creamos un modelo y lo guardamos
+        const $ = cheerio.load(html);
+        const tweets = $('.tweet');
 
         let numSavedTweets = 0;
 
-        // paso 4: para cada tweet, creamos un modelo y lo guardamos
         tweets.each(function () {
             const user = $(this).find('.fullname').text();
             const body = $(this).find('.tweet-text').text();
@@ -65,40 +72,6 @@ db.once('open', function () {
         console.log("¡" + numSavedTweets + " tweets guardados!");
 
     })();
-
-    /*
-
-    METODO ANTIGUO: FETCH CON AXIOS
-
-    axios(url)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            const tweets = $('.tweet');
-
-            let numSavedTweets = 0;
-
-            // paso 4: para cada tweet, creamos un modelo y lo guardamos
-            tweets.each(function() {
-                const user = $(this).find('.fullname').text();
-                const body = $(this).find('.tweet-text').text();
-                const date = $(this).find('._timestamp').attr('data-time');
-
-                let tweetToSave = new Tweet({user: user, body: body, date: date});
-
-                tweetToSave.save(function (err, tweetToSave) {
-                    if (err) return console.error(err);
-                });
-
-                numSavedTweets++;
-
-            });
-
-            console.log("¡" + numSavedTweets + " tweets guardados!");
-
-        })
-        .catch(console.error);
-    */
 
 });
 
